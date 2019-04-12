607f558bc4402f252fecb919f604c1390fd5337b
Joao Tonial <joao.tonial@sbsistemas.com.br>
2019-04-12 13:36:26 -0300

    adicionando teste de unidade para o suporte a codigo ibge

--- ie.js
+++ ie.js
@@ -552,46 +552,46 @@ function lookup(ie){
 }
 
 var codigosIBGE = {
-  11:	'RO',
-  12:	'AC',
-  13:	'AM',
-  14:	'RR',
-  15:	'PA',
-  16:	'AP',
-  17:	'TO',
-  21:	'MA',
-  22:	'PI',
-  23:	'CE',
-  24:	'RN',
-  25:	'PB',
-  26:	'PE',
-  27:	'AL',
-  28:	'SE',
-  29:	'BA',
-  31:	'MG',
-  32: 'ES',
-  33: 'RJ',
-  35:	'SP',
-  41:	'PR',
-  42:	'SC',
-  43:	'RS',
-  50:	'MS',
-  51:	'MT',
-  52:	'GO',
-  53:	'DF',
-}
+    11:	'RO',
+    13:	'AM',
+    12:	'AC',
+    14:	'RR',
+    15:	'PA',
+    16:	'AP',
+    17:	'TO',
+    21:	'MA',
+    22:	'PI',
+    23:	'CE',
+    24:	'RN',
+    25:	'PB',
+    26:	'PE',
+    27:	'AL',
+    28:	'SE',
+    29:	'BA',
+    31:	'MG',
+    32: 'ES',
+    33: 'RJ',
+    35:	'SP',
+    41:	'PR',
+    42:	'SC',
+    43:	'RS',
+    50:	'MS',
+    51:	'MT',
+    52:	'GO',
+    53:	'DF',
+};
 
 function validar(ie, estado){
-    if(!isNaN(estado)) {
-      estado = codigosIBGE[code];
-      if(eIndefinido(estado)){
-        throw new Error('código não é valido')
-      }
+    if(estado && !isNaN(estado)) {
+        if(!Object.keys(codigosIBGE).includes(estado)){
+            throw new Error('código não é valido');
+        }
+        estado = codigosIBGE[estado];
     }
 
     if(eIndefinido(estado) || estado === null){
-      estado = '';
-    } 
+        estado = '';
+    }
 
     estado = estado.toLowerCase();
 
